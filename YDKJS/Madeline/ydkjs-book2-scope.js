19f69959ceccd234948db73899a156f139c3d35e
Donnell Jenkins <donnell_jenkins@hotmail.com>
2019-07-15 15:52:50 -0500

    initial

---
+++ YDKJS/Madeline/ydkjs-book2-scope.js
@@ -0,0 +1,10 @@
+console.log("This file demonstrates key points from the YDKJS book series, Book 2 Scope & Closures, Chapter 3: Function vs Block Scope:");
+
+// create a global object (namespace) that has
+//  - a function that retrieves all reviews on the site
+
+// write IIFE 
+//  - that calls the get all reviews function from the global object (namespace)
+//  - store the results of the function call to a const
+//  - loop through the reviews and output the restaurant name and it's rating
+console.log("show reviews here ....");
